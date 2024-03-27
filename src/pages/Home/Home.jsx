c733fea421ef449ac8fba375e9dca5b3c9944fa3
al-amin90 <ijesun30@gmail.com>
2024-03-27 15:34:10 +0600

    loader added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
+import { useNavigation } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
 import BookLists from "../../components/BookLists/BookLists";
+import Loader from "../Loader/Loader";
 
 
 const Home = () => {
+    const navigation = useNavigation();
+
+    if (navigation.state === "loading") return <Loader></Loader>
     return (
         <div>
             <Banner></Banner>
